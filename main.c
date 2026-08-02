0b93ed84ad6eb8bd4a127be35d5666775912d6df
mrghost077 <msnlms86@gmail.com>
2026-08-02 08:55:04 +0530

    Initializing the project

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+----------------------------------------------------------
+EEI5265 - Operating Systems Mini Project
+Group A - Page Table Translator
+
+Description:
+This program simulates the translation of logical addresses
+into physical addresses using one-level paging.
+
+Author: Safwan
+Date: August 2026
+----------------------------------------------------------
+*/
